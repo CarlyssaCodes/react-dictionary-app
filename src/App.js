05e311f2acaa50705c63ebe997abd493b3040f11
Carlyssa <78633277+CarlyssaCodes@users.noreply.github.com>
2021-09-19 17:17:20 -0400

    set up dictionary component and set up search form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Dictionary App</h1>
-      <button className="btn btn-primary">Search Word</button>
-      <footer>
-        <a href="https://github.com/CarlyssaCodes/react-weather-app">
-          Open-source
-        </a>{" "}
-        coded by Carlyssa Pierre
-      </footer>
+    <div className="App">
+      <div className="container">
+        <header className="App-header" />
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <a href="https://github.com/CarlyssaCodes/react-weather-app">
+            Open-source
+          </a>{" "}
+          coded by Carlyssa Pierre
+        </footer>
+      </div>
     </div>
   );
 }
