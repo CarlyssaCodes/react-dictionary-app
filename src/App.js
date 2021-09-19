2c5cddf37611f046364dd842a05d0492d954a694
Carlyssa <78633277+CarlyssaCodes@users.noreply.github.com>
2021-09-18 22:05:25 -0400

    repository linked

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
     <div>
       <h1>Dictionary App</h1>
       <button className="btn btn-primary">Search Word</button>
+      <footer>
+        <a href="https://github.com/CarlyssaCodes/react-weather-app">
+          Open-source
+        </a>{" "}
+        coded by Carlyssa Pierre
+      </footer>
     </div>
   );
 }
