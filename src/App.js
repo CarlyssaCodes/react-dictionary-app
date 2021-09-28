15cda8a99e6b0f851c1d462f60458163859ec251
Carlyssa <78633277+CarlyssaCodes@users.noreply.github.com>
2021-09-27 22:05:02 -0400

    made improvements to overall css

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ export default function App() {
       <div className="container">
         <header className="App-header" />
         <main>
-          <h1>Dictionary App</h1>
+          <h1>Dictionary</h1>
           <Dictionary defaultKeyword="determination" />
         </main>
         <footer className="App-footer">
-          <a href="https://github.com/CarlyssaCodes/react-weather-app">
-            Open-source
-          </a>{" "}
-          coded by Carlyssa Pierre
+          <small>
+            <a href="https://github.com/CarlyssaCodes/react-weather-app">
+              Open-source
+            </a>{" "}
+            coded by Carlyssa Pierre. Photos provided by{" "}
+            <a href="https://www.pexels.com">Pexels</a> and hosted on{" "}
+            <a href="https://www.netlify.com">Netlifly</a>
+          </small>
         </footer>
       </div>
     </div>
